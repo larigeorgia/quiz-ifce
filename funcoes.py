0c2710e28b8be7ca66f25f55865a7594ef021531
georgia_segurobet <larigeorgiab@gmail.com>
2026-06-18 21:35:15 -0300

    Cria esboço das funções, edita main e perguntas

---
+++ funcoes.py
@@ -0,0 +1,13 @@
+# Parte 2 — funcoes.py
+# Transforme a lógica que estava misturada no seu código em funções com responsabilidade única. No mínimo, você deve ter:
+
+# Função	Responsabilidade
+# exibir_menu()	Exibe o menu e retorna a opção escolhida pelo usuário
+# fazer_pergunta(pergunta)	Exibe uma pergunta, recebe e valida a resposta, retorna True ou False
+# Cada função deve ter uma docstring
+
+def exibir_menu():
+    print("teste-exibir menu")
+
+def fazer_pergunta():
+    print("teste-pergunta")
